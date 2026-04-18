906016af0966480e6483771a41adde37983afde3
Robbie Leslie <rwl228@cornell.edu>
2026-04-18 18:44:35 -0400

    Added microros to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,29 @@
 #include <Arduino.h>
 #include <Wire.h>
 
-
 #include "LightFunctions.h"
 #include "DistanceFunctions.h"
+#include "microRosFunctions.h"
 
 
 void setup() {
   Serial.begin(115200);
   unsigned long start = millis();
   while (!Serial && millis() - start < 10000) delay(10);
-  Serial.println("Hello world!\n");
 
   setupLight();
   setLight(true);
   setupTof();
+
+#ifdef ROS
+  microRosSetup();
+#endif
 }
 
 void loop() {
+#ifdef ROS
+  microRosTick();
+#else
   Serial.printf("FRONT: %d mm\r\n", getDistance(FRONT));
   Serial.printf("LEFT:  %d mm\r\n", getDistance(LEFT));
   Serial.printf("RIGHT: %d mm\r\n", getDistance(RIGHT));
@@ -25,4 +31,5 @@ void loop() {
   Serial.printf("EXTRA: %d mm\r\n", getDistance(EXTRA));
   Serial.print("\r\n");
   delay(500);
+#endif
 }
